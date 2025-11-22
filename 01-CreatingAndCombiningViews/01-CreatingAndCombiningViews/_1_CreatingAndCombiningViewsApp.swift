97f21ee86ce3c6c3bcfb211707b117b52afc43bc
Aziz Arfaoui <aziz99arfaoui@gmail.com>
2025-11-22 01:06:37 +0100

    Add .gitignore and remove user-specific files from Git

---
+++ 01-CreatingAndCombiningViews/01-CreatingAndCombiningViews/_1_CreatingAndCombiningViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_CreatingAndCombiningViewsApp.swift
+//  01-CreatingAndCombiningViews
+//
+//  Created by Aziz Arfaoui on 22/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct _1_CreatingAndCombiningViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
